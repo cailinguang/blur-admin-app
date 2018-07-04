2929c12d9782e3a33c4352a905bfcdb6c7594359
cailinguang <user_cai@126.com>
2018-07-04 10:51:05 +0800

    commit

--- src/app/pages/evaluation/EvaluationChartCtrl.js
+++ src/app/pages/evaluation/EvaluationChartCtrl.js
@@ -22,17 +22,22 @@
             var labels = [];
             var data = [];
             var allScope = 0;
+            var allScopeIndex = 0;
             if(response.data.code==200){
                 response.data.data.forEach(function(e){
                     labels.push(e.name);
                     data.push(e.scope.toFixed(2));
-                    allScope = allScope+e.scope;
+                    //allScope = allScope+e.scope;
+                    e.children.forEach(function(c){
+                        allScope+=c.scope;
+                        allScopeIndex++;
+                    });
                 });
                 $scope.labels = labels;
                 $scope.datas.push(data);
 
                 $scope.nodes = response.data.data;
-                $scope.allScope = allScope/response.data.data.length;
+                $scope.allScope = allScope/allScopeIndex;
             }
         });
     };
